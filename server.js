b33ac49c9701a10c456ea8394f8a03269860febf
safriansah <safriansah@usahakreatif.co.id>
2021-06-07 09:45:09 +0700

    push 10 minute cronjob

--- server.js
+++ server.js
@@ -65,77 +65,19 @@ fastify.get('/config/update/:type', async function(req, res){
   res.send('update::' + result);
 })
 
-const databases = require('./databaseList.json');
-const CronJob = require('cron').CronJob;
-let job1 = new CronJob('0 */5 * * * *', async function() {
-  console.log('cronjob-1::' + new Date());
-  for(let database of databases){
-    // console.log("database::", database);
-    await socketService.checkDatabase(database);
-  }
-}, null, true, 'Asia/Jakarta'); 
-job1.start();
-// async function actionPost(data) {
-//   try {
-//     axios({
-//       method: "post",
-//       url: data.url,
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       data: data.content,
-//     });
-//   } catch (err) {
-//     console.log("error action post");
-//   }
-// }
-
-
-// fastify.post("/internalAccess/:params/:accountName", async (request, reply) => {
-//   console.log();
-//   let data = {};
-//   let p = request.params.params;
-//   let acn = request.query.accountName;
-//   data.content = request.body;
-//   console.log(data);
-//   // switch(p){
-//   //   case "register":
-//   //     data.url = {url:process.env.ACCOUNT_SERVICE_HOST+acn,}
-//   //     let a = await actionPost()
-//   //     break;
-//   // }
-//   return p;
-// });
-
-// fastify.post("/login/authentications/:params", async (request, reply) => {
-//   let url =
-//     "http://localhost:8202/authentications/login/" + request.params.params;
-//   axios({
-//     method: "post",
-//     url: url,
-//     data: {
-//       params: {
-//         email: request.body.params.email,
-//         password: request.body.params.password,
-//       },
-//     },
-//   })
-//     .then(async function (response) {
-//       console.log("=========>", response);
-//       // try{
-//       //   await createCKA(response)
-//       // }
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-//   return { hello: "pak eko" };
-// });
-
 const start = async () => {
   try {
     fastify.use(helmet());
-
+    const databases = require('./databaseList.json');
+    const CronJob = require('cron').CronJob;
+    let job1 = new CronJob('0 */10 * * * *', async function() {
+      console.log('cronjob-1::' + new Date());
+      for(let database of databases){
+        // console.log("database::", database);
+        await socketService.checkDatabase(database);
+      }
+    }, null, true, 'Asia/Jakarta'); 
+    job1.start();
     let a = await fastify.listen(8209, "0.0.0.0");
     // io.on('connection', async function (socket) {
     //   console.log('Socket Connect::' + socket.id);
